e8bc9a844516c229d3e63e0a12e42a42afccb901
Alexandr Korsak <alex.korsak@gmail.com>
2015-11-21 14:50:18 +0300

    Add package and description information

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule RedisPoolex.Mixfile do
     [app: :redis_poolex,
      version: "0.0.1",
      elixir: "~> 1.2-dev",
+     description: description,
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -17,6 +19,24 @@ defmodule RedisPoolex.Mixfile do
     [
       applications: [:logger],
       mod: {RedisPoolex, []}
+    ]
+  end
+
+  defp description do
+    """
+    Redis connection pool using poolboy and exredis libraries
+    """
+  end
+
+  defp package do
+    [# These are the default files included in the package
+      files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+      maintainers: ["Alexandr Korsak"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/oivoodoo/redis_poolex",
+        "Docs" => "http://oivoodoo.github.io/redis_poolex/"
+      }
     ]
   end
 
