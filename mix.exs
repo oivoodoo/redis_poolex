34628d0a73f73c9ef848c1868ce9b5868a8133c3
Alexandr Korsak <alex.korsak@gmail.com>
2015-11-27 14:06:51 +0300

    Bump version to 0.0.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RedisPoolex.Mixfile do
 
   def project do
     [app: :redis_poolex,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.2-dev",
      description: description,
      package: package,
